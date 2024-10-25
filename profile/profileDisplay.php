018209b85aee0a42a0b493ccd80679022ebf13e7
HexHavoc <iamanirudh6@gmail.com>
2024-10-25 20:40:28 +0530

    removed the title element from the html section so that each category can show their respective titles

--- profile/profileDisplay.php
+++ profile/profileDisplay.php
@@ -8,14 +8,13 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
 <meta name="description" content="">
 <link rel="stylesheet" href="../profile/profileDisplay.css">
-<title>Login</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 <div class="topmenu">
 	<div class="menubar">
-	  	<?php if (isset($_SESSION['logged_in'])) {  ?>
+	  	<?php if (isset($_SESSION['logged_in'])) { ?>
 	  	<div class="logout">
 	  		<a href="../profile/profileLogout.php"><i class="fa fa-sign-out"></i> Logout</a>
 	  	</div>
